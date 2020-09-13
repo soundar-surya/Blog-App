779c8d9269bc7d1f40bb8710d887a5e93830c00a
soundar surya <soundarsurya58@gmail.com>
2020-09-13 20:16:38 +0530

    redis integration part-1

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,9 +14,28 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
 
+    const redis = require('redis');
+    const redisUrl = "redis://127.0.0.1:6379"
+    const client = redis.createClient(redisUrl);
+    const util = require('util');
+    client.get = util.promisify(client.get);
+
+    //check redis, is there any related data to this query
+    const cachedBlog = await client.get(req.user.id);
+    
+    if(cachedBlog) {
+      console.log('SERVING FROM CACHE');
+     return  res.send(JSON.parse(cachedBlog));
+    }
+
+    //otherwise, respond to the req with mongo and store the data in cache
+    const blogs = await Blog.find({ _user: req.user.id });
+    console.log('SERVING FROM MONGODB');
     res.send(blogs);
+
+     //storing data in cache
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
